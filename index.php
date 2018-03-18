574ab830f33901236dc2a5063b3bcc0a83908023
niddyng <niddyng@gmail.com>
2018-03-18 14:01:20 +0700

    layout homepage

---
+++ index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>NT Store - Nơi mua bán điện thoại, tablet, laptop, phụ kiện tốt nhất</title>
+
+	<!-- Latest compiled and minified CSS & JS -->
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="css/homestyle.css">
+	<script src="jquery/jquery.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+	<div class="container-fluid">
+		<header>
+			<div class="container">
+				<div class="col-md-3">
+					<img src="img/LOGO.png">
+				</div>
+				<div class="col-md-1"></div>
+				<div class="col-md-4 margintop10">
+					<div class="input-group">
+      					<input type="text" class="form-control" placeholder="Search ....">
+      					<span class="input-group-btn">
+        					<button class="btn btn-default" type="button">
+        						<span class="glyphicon glyphicon-search"></span>
+        					</button>
+      					</span>
+    				</div>
+				</div>
+				<div class="col-sm-2"></div>
+				<div class="col-md-2">
+					<div class="dropdown margintop10">
+						<div class="btn-group">
+    						<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+      							Tài khoản&nbsp;
+      							<span class="glyphicon glyphicon-user"></span>
+    						</button>
+    						<ul class="dropdown-menu">
+      							<li><a href="#">Đăng nhập</a></li>
+      							<li><a href="#">Đăng ký</a></li>
+    						</ul>
+  						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">
+						<span class="glyphicon glyphicon-home">
+							Home
+						</span>
+					</a>
+				</div>
+		
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="#">Điện thoại</a></li>
+						<li><a href="#">Máy tính bảng</a></li>
+						<li><a href="#">Laptop</a></li>
+						<li><a href="#">Phụ kiện</a></li>
+					</ul>
+						
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#">Khuyến mãi</a></li>
+						<li><a href="#">Hỏi đáp</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Tuyển dụng <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Nộp đơn ứng tuyển</a></li>
+								<li><a href="#">Qui Trình</a></li>
+								<li><a href="#">Cơ hội nghề nghiệp cho Sinh viên</a></li>
+								<li><a href="#">Những câu hỏi thường gặp</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div>
+		</nav>
+		
+		<article class="container">
+			<section class="col-md-9">
+				<h2>Bán chạy nhất</h2>
+				<div class="row">
+  					<div class="col-sm-6 col-md-4">
+    					<div class="thumbnail">
+      						<img src="img/iphoneX.jpeg" alt="iphoneX">
+      						<div class="caption">
+        						<h3>iPhone X - 64GB</h3>
+       								<p>Apple - iPhone X - 64GB</p>
+        						<p>
+        							<a href="#" class="btn btn-primary" role="button">
+        								Chi tiết
+        							</a> 
+        							<a href="#" class="btn btn-default" role="button">
+        							<span class="glyphicon glyphicon-shopping-cart"></span>
+        							Thêm vào giỏ
+        							</a>
+        						</p>
+      						</div>
+    					</div>
+  					</div>
+
+  					<div class="col-sm-6 col-md-4">
+    					<div class="thumbnail">
+      						<img src="img/iphoneX.jpeg" alt="iphoneX">
+      						<div class="caption">
+        						<h3>iPhone X - 64GB</h3>
+       								<p>Apple - iPhone X - 64GB</p>
+        						<p>
+        							<a href="#" class="btn btn-primary" role="button">
+        								Chi tiết
+        							</a> 
+        							<a href="#" class="btn btn-default" role="button">
+        							<span class="glyphicon glyphicon-shopping-cart"></span>
+        							Thêm vào giỏ
+        							</a>
+        						</p>
+      						</div>
+    					</div>
+  					</div>
+
+  					<div class="col-sm-6 col-md-4">
+    					<div class="thumbnail">
+      						<img src="img/iphoneX.jpeg" alt="iphoneX">
+      						<div class="caption">
+        						<h3>iPhone X - 64GB</h3>
+       								<p>Apple - iPhone X - 64GB</p>
+        						<p>
+        							<a href="#" class="btn btn-primary" role="button">
+        								Chi tiết
+        							</a> 
+        							<a href="#" class="btn btn-default" role="button">
+        							<span class="glyphicon glyphicon-shopping-cart"></span>
+        							Thêm vào giỏ
+        							</a>
+        						</p>
+      						</div>
+    					</div>
+  					</div>
+				</div>
+				
+			</section>
+
+			<aside class="col-md-3">
+				<h3>Gợi ý sản phẩm</h3>
+				<div class="media">
+  					<div class="media-left media-middle">
+    					<a href="#">
+      					<img class="media-object" src="img/iphoneX.jpeg" alt="iphoneX" width="64" height="64">
+    					</a>
+  					</div>
+ 	 				<div class="media-body">
+    				<h4 class="media-heading">iPhone X - 64GB</h4>
+    					...
+  					</div>
+				</div>
+			</aside>
+		</article>
+
+		<footer class="container-fluid text-center">
+			<p>Copyright &copy; 2018 NT Store</p>
+			<h4>Contact</h4>
+			<p>14520615<br>14520688</p>
+		</footer>
+	</div>
+</body>
+</html>
